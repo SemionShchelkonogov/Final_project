7a3c66b548563ccc3e1dbe05415d902d582d7467
mac <mac@MacBook-Air-mac.local>
2022-12-11 15:15:19 +0600

    Add an array element

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string [] GetTextFromUser (int length)
+{
+string[] array = new string [length];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите {i+1} элемент массива: ");
+    array[i] = (Console.ReadLine()??"0");
+}
+return array;
+}
